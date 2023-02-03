43ad17ef6704d9a3c7d320f0c76ae59875b19eea
Deadly844119 <103536456+Deadly844119@users.noreply.github.com>
2023-02-03 21:13:04 +0530

    fix scrolling

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
 const bar = document.getElementById("bar");
-const menu = document.getElementById("menu")
+const menu = document.getElementById("menu");
+const body = document.getElementById('body');
 const showMenu = () => {
-    if(menu.style.right=="100%")
+    if(menu.style.left=="0%")
     {
-        menu.style.right="0%"
+        menu.style.left="-100%"
+        body.style.overflow="auto"
     }
     else{
-        menu.style.right="100%"
+        menu.style.left="0%"
+        body.style.overflow="hidden"
     }
 }
 bar.addEventListener('click',showMenu)
