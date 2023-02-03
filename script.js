2e9f5143c56efb434a6a2522b78de195f9024ca0
Deadly844119 <103536456+Deadly844119@users.noreply.github.com>
2023-02-03 21:17:58 +0530

    error fix

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 const bar = document.getElementById("bar");
 const menu = document.getElementById("menu");
 const body = document.getElementById('body');
+const rootele = document.getElementsByTagName('html')
 const showMenu = () => {
     if(menu.style.left=="0%")
     {
         menu.style.left="-100%"
         body.style.overflow="auto"
+        rootele.style.overflow="auto"
     }
     else{
         menu.style.left="0%"
         body.style.overflow="hidden"
+        rootele.style.overflow="hidden"
     }
 }
 bar.addEventListener('click',showMenu)
